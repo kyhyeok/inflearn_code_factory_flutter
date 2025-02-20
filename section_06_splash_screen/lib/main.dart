1fe2f7b1675ef0aa8c832875f10141e81729a34a
김윤혁 <kyhyeok@gmail.com>
2025-02-20 13:24:24 +0900

    Splash Screen -Column 위젯 사용해보기

--- section_06_splash_screen/lib/main.dart
+++ section_06_splash_screen/lib/main.dart
@@ -6,7 +6,14 @@ void main() {
       home: Scaffold(
         /// 335CB0
         backgroundColor: Color(0xFF335CB0),
-        body: Image.asset('asset/img/logo.png'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo.png',
+            ),
+          ],
+        ),
       ),
     ),
   );

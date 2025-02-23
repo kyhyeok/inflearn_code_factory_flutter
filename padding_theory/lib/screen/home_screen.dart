edb20f0ec68e9e90fdbd715ddedd5279d41169e8
김윤혁 <kyhyeok@gmail.com>
2025-02-23 23:13:50 +0900

    [이론] Padding - init

---
+++ padding_theory/lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.red,
+          child: Container(
+            color: Colors.blue,
+            width: 50.0,
+            height: 50.0,
+          ),
+        ),
+      ),
+    );
+  }
+}

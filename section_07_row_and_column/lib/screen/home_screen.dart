ad56071026c53fad7c0ad19b28c69a59604bbfab
김윤혁 <kyhyeok@gmail.com>
2025-02-23 22:44:20 +0900

    row & column - Expanded Widget

--- section_07_row_and_column/lib/screen/home_screen.dart
+++ section_07_row_and_column/lib/screen/home_screen.dart
@@ -11,15 +11,31 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           color: Colors.black,
           child: Column(
-            children: colors
-                .map(
-                  (e) => Container(
-                    height: 50.0,
-                    width: 50.0,
-                    color: e,
-                  ),
-                )
-                .toList(),
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 50.0,
+                  width: 50.5,
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  height: 50.0,
+                  width: 50.5,
+                  color: Colors.orange,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 50.0,
+                  width: 50.5,
+                  color: Colors.yellow,
+                ),
+              ),
+            ],
           ),
         ),
       ),

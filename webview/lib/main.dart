ee2953a8364cb914813ee651e533c2dfda4d78a9
김윤혁 <kyhyeok@gmail.com>
2025-02-24 00:56:43 +0900

    블로그웹앱 webview - Pub.dev 구경 및 외부 패키지 추가해보기

--- webview/lib/main.dart
+++ webview/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:webview/screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }

04f13d174d51937b65159e0f281d8e9e52b4fd0c
김윤혁 <kyhyeok@gmail.com>
2025-02-24 00:45:04 +0900

    블로그웹앱 webview - init

--- padding_theory/lib/screen/home_screen.dart
+++ padding_theory/lib/screen/home_screen.dart
@@ -9,10 +9,18 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Container(
           color: Colors.red,
-          child: Container(
-            color: Colors.blue,
-            width: 50.0,
-            height: 50.0,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+              32.0,
+              64.0,
+              16.0,
+              8.0,
+            ),
+            child: Container(
+              color: Colors.blue,
+              width: 50.0,
+              height: 50.0,
+            ),
           ),
         ),
       ),

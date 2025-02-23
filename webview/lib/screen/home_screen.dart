57c2ab022bedb179609ecc321bf9fddd4ea7d0d5
김윤혁 <kyhyeok@gmail.com>
2025-02-24 01:39:54 +0900

    블로그웹앱 webview - Appbar 디자인하기

--- webview/lib/screen/home_screen.dart
+++ webview/lib/screen/home_screen.dart
@@ -5,6 +5,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hyeok'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
